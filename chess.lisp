db1ab8f50dfd0a8b8282c5efcc0ca8a890566d19
Perry <thescratcherman@gmail.com>
2026-04-02 13:43:13 -0700

    hooked into the right event

--- chess.lisp
+++ chess.lisp
@@ -16,27 +16,28 @@
   nil)
 (ql:quickload :cl-gtk2-gtk)
 
-
-
 ;; Ok, let's render a chessboard.
-(gtk:within-main-loop
-  (let ((window (make-instance 'gtk:gtk-window
-                               :type :toplevel
-                               :title "Hello World"
-                               :default-width 300
-                               :default-height 200))
-        (button (make-instance 'gtk:button
-                               :label "Hello, World!")))
-    ;; Close the window when the user clicks the X
-    (gobject:connect-signal window "destroy"
-                            (lambda (widget)
-                              (declare (ignore widget))
-                              (gtk:leave-gtk-main)))
-    ;; Print a message when the button is clicked
-    (gobject:connect-signal button "clicked"
-                            (lambda (widget)
-                              (declare (ignore widget))
-                              (format t "Hello, World!~%")))
-    ;; Add the button to the window and show everything
-    (gtk:container-add window button)
-    (gtk:widget-show window :all t)))
+(defun test ()
+  (gtk:within-main-loop
+    (let ((window (make-instance 'gtk:gtk-window
+                                 :type :toplevel
+                                 :title "Hello World"
+                                 :default-width 300
+                                 :default-height 200))
+          (drawing-area (make-instance 'gtk:drawing-area)))
+
+      ;; Close the window when the user clicks the X
+      (gobject:connect-signal window "destroy"
+                              (lambda (widget)
+                                (declare (ignore widget))
+                                (gtk:leave-gtk-main)))
+
+      (gobject:connect-signal drawing-area "expose-event"
+                              (lambda (widget event)
+                                (declare (ignore widget))
+                                (format t "Exposed!~%")))
+
+      ;; Add the button to the window and show everything
+      (gtk:container-add window drawing-area)
+      (gtk:widget-show window :all t))))
+(test)
