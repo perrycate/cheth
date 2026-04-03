f7b0ae7cc91c0899fa2fdd769b153fc1eaedc905
Perry <thescratcherman@gmail.com>
2026-04-03 14:40:42 -0700

    hey we got a chessboard

--- chessboard.lisp
+++ chessboard.lisp
@@ -1,16 +1,26 @@
-;; Following a 15 year old tutorial, let's render an image to a pdf using Cairo 2.
 (asdf:operate 'asdf:load-op :cl-cairo2)
 (use-package :cl-cairo2)
-(defparameter *surface* (create-pdf-surface "test.pdf" 200 100))
+(defparameter *surface* (create-pdf-surface "test.pdf" 200 200))
 (setf *context* (create-context *surface*))
-(set-source-rgb 0.2 0.2 1)
+
+;; This should all be painted over, so the red is just to highlight if something is misaligned.
+(set-source-rgb 1 0 0)
 (paint)
 
-;; Draw a white diagonal line.
-(move-to 200 0)
-(line-to 0 100)
-(set-source-rgb 1 1 1)
-(set-line-width 5)
-(stroke)
+(defun draw-board (width)
+  (let ((square-width (/ width 8)))
+    (dotimes (x 8)
+      (dotimes (y 8)
+        (rectangle (* x square-width) (* y square-width) square-width square-width)
+        ;; Alternate square colors.
+        (if (= (mod (+ x y) 2) 0) (set-source-rgb 1 1 1) (set-source-rgb 0 0 0))
+        (fill-path)
+        ))
+    (set-source-rgb 1 1 1)
+    (set-line-width 5)
+    (fill-path)
+    ))
+
+(draw-board 200)
 (destroy *context*)
 (destroy *surface*)
