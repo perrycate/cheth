3f2e1e0d189f0dd92b5e5beedbbd23fb0efffb6e
Perry <thescratcherman@gmail.com>
2026-04-06 14:02:37 -0700

    placement is wrong, but we're close

--- chess.lisp
+++ chess.lisp
@@ -19,35 +19,6 @@
 
 (use-package :cl-cairo2)
 
-(dolist (file (directory "pieces/*.lisp"))
-  (load file))
-
-(defun draw-board (width)
-  (let ((square-width (/ width 8)))
-    (dotimes (x 8)
-      (dotimes (y 8)
-        (rectangle (* x square-width) (* y square-width) square-width square-width)
-        ;; Alternate square colors.
-        (if (= (mod (+ x y) 2) 0) (set-source-rgb 1 1 1) (set-source-rgb 0 0 0))
-        (fill-path)
-        ))
-
-    ;; Render the pieces. Don't care about placement, just want to see if they look ok.
-    (draw-white-pawn 0 0 100)
-    (draw-white-bishop 0 100 100)
-    (draw-white-knight 0 200 100)
-    (draw-white-rook 0 300 100)
-    (draw-white-queen 0 400 100)
-    (draw-white-king 0 500 100)
-
-    (draw-black-pawn 100 0 100)
-    (draw-black-bishop 100 100 100)
-    (draw-black-knight 100 200 100)
-    (draw-black-rook 100 300 100)
-    (draw-black-queen 100 400 100)
-    (draw-black-king 100 500 100)
-    ))
-
 (defun render-chessboard (widget event)
   (declare (ignore event))
   (setf *context* (cl-gtk2-cairo:create-gdk-context (gtk:widget-window widget)))
@@ -60,23 +31,23 @@
 ;; Ok, let's render a chessboard.
 (defun test ()
   (gtk:within-main-loop
-    (let ((window (make-instance 'gtk:gtk-window
-                                 :type :toplevel
-                                 :title "Hello World"
-                                 :default-width 300
-                                 :default-height 200))
-          (drawing-area (make-instance 'gtk:drawing-area)))
+   (let ((window (make-instance 'gtk:gtk-window
+                                :type :toplevel
+                                :title "Hello World"
+                                :default-width 300
+                                :default-height 200))
+         (drawing-area (make-instance 'gtk:drawing-area)))
 
-      ;; Close the window when the user clicks the X
-      (gobject:connect-signal window "destroy"
-                              (lambda (widget)
-                                (declare (ignore widget))
-                                (gtk:leave-gtk-main)))
+     ;; Close the window when the user clicks the X
+     (gobject:connect-signal window "destroy"
+                             (lambda (widget)
+                               (declare (ignore widget))
+                               (gtk:leave-gtk-main)))
 
-      (gobject:connect-signal drawing-area "expose-event"
-                              #'render-chessboard)
+     (gobject:connect-signal drawing-area "expose-event"
+                             #'render-chessboard)
 
-      ;; Add the button to the window and show everything
-      (gtk:container-add window drawing-area)
-      (gtk:widget-show window :all t))))
+     ;; Add the button to the window and show everything
+     (gtk:container-add window drawing-area)
+     (gtk:widget-show window :all t))))
 (test)
--- chessboard.lisp
+++ chessboard.lisp
@@ -1,12 +1,80 @@
 (asdf:operate 'asdf:load-op :cl-cairo2)
 (use-package :cl-cairo2)
-(defparameter *surface* (create-pdf-surface "test.pdf" 200 200))
+(defparameter *surface* (create-pdf-surface "test.pdf" 600 600))
 (setf *context* (create-context *surface*))
 
-;; This should all be painted over, so the red is just to highlight if something is misaligned.
+(defparameter *starting-position*
+  '(
+    (:a1 . (:white :rook))
+    (:b1 . (:white :knight))
+    (:c1 . (:white :bishop))
+    (:d1 . (:white :queen))
+    (:e1 . (:white :king))
+    (:f1 . (:white :bishop))
+    (:g1 . (:white :knight))
+    (:h1 . (:white :rook))
+
+    (:a2 . (:white :pawn))
+    (:b2 . (:white :pawn))
+    (:c2 . (:white :pawn))
+    (:d2 . (:white :pawn))
+    (:e2 . (:white :pawn))
+    (:f2 . (:white :pawn))
+    (:g2 . (:white :pawn))
+    (:h2 . (:white :pawn))
+
+
+    (:a7 . (:black :pawn))
+    (:b7 . (:black :pawn))
+    (:c7 . (:black :pawn))
+    (:d7 . (:black :pawn))
+    (:e7 . (:black :pawn))
+    (:f7 . (:black :pawn))
+    (:g7 . (:black :pawn))
+    (:h7 . (:black :pawn))
+
+    (:a8 . (:black :rook))
+    (:b8 . (:black :knight))
+    (:c8 . (:black :bishop))
+    (:d8 . (:black :queen))
+    (:e8 . (:black :king))
+    (:f8 . (:black :bishop))
+    (:g8 . (:black :knight))
+    (:h8 . (:black :rook))
+    ))
+
+;; The whole drawing surface be painted over,
+;; so this red is just to highlight if something is misaligned.
 (set-source-rgb 1 0 0)
 (paint)
 
+;; One day I will figure out how to actually make this all a cohesive package.
+;; Today is not that day.
+(dolist (file (directory "pieces/*.lisp"))
+  (load file))
+
+(defun draw-piece (color piece pixel-coordinates width)
+  (destructuring-bind (x y) pixel-coordinates
+    (cond
+      ((and (eq color :white) (eq piece :pawn)) (draw-white-pawn x y width))
+      ((and (eq color :white) (eq piece :knight)) (draw-white-knight x y width))
+      ((and (eq color :white) (eq piece :bishop)) (draw-white-bishop x y width))
+      ((and (eq color :white) (eq piece :rook)) (draw-white-rook x y width))
+      ((and (eq color :white) (eq piece :queen)) (draw-white-queen x y width))
+      ((and (eq color :white) (eq piece :king)) (draw-white-king x y width))
+
+      ((and (eq color :black) (eq piece :pawn)) (draw-black-pawn x y width))
+      ((and (eq color :black) (eq piece :knight)) (draw-black-knight x y width))
+      ((and (eq color :black) (eq piece :bishop)) (draw-black-bishop x y width))
+      ((and (eq color :black) (eq piece :rook)) (draw-black-rook x y width))
+      ((and (eq color :black) (eq piece :queen)) (draw-black-queen x y width))
+      ((and (eq color :black) (eq piece :king)) (draw-black-king x y width))
+      )))
+
+;; TODO I wonder if I can just get width from the context?
+;; Would need to call gdk:drawable-get-size on the widget window.
+;; The context takes the widget window as a parameter, so I bet I can
+;; get it.
 (defun draw-board (width)
   (let ((square-width (/ width 8)))
     (dotimes (x 8)
@@ -16,11 +84,28 @@
         (if (= (mod (+ x y) 2) 0) (set-source-rgb 1 1 1) (set-source-rgb 0 0 0))
         (fill-path)
         ))
-    (set-source-rgb 1 1 1)
-    (set-line-width 5)
-    (fill-path)
     ))
 
-(draw-board 200)
+;; Translates a square value (eg :b2) into x and y pixel coordinates for a board of the given size.
+(defun get-coordinates (square board-width)
+  ;; 0,0 is in the top left corner.
+  (let* (
+         (square-width (/ board-width 8))
+         (s (string square))
+         ;; Start by getting offsets, where each square is one unit.
+         (x-offset (- (char-int (char s 0)) (char-int #\A)))
+         (y-offset (- (digit-char-p (char s 1)) 1)))
+
+    ;; Scale to board width.
+    (list (* x-offset square-width) (* y-offset square-width))))
+
+(defun draw-position (position width)
+  (dolist (piece position)
+    (destructuring-bind (square . (color piece)) piece
+      (draw-piece color piece (get-coordinates square width) (/ width 8)))))
+
+(draw-board 600)
+(draw-position *starting-position* 600)
+
 (destroy *context*)
 (destroy *surface*)
